5a829aac4ee6e1209effa8bdc4e6944ea416742c
aymanalqadhi <alqd@protonmail.com>
2022-04-23 23:54:57 +0300

    Add basic user entity

---
+++ APSS.Domain.Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Domain.Entities;
+
+public sealed class User : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the number of the users
+    /// </summary>
+    public string UserNumber { get; set; } = null!;
+}
