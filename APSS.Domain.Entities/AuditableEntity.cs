41150c4f9fab79156373f3a86c13152246952f64
aymanalqadhi <alqd@protonmail.com>
2022-04-23 23:54:49 +0300

    Add entity base class with audition properties

---
+++ APSS.Domain.Entities/AuditableEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace APSS.Domain.Entities;
+
+public abstract class AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the id of the entity
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the creation date of the entity
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+
+    /// <summary>
+    /// Gets or sets the last modification date of the entity
+    /// </summary>
+    public DateTime ModifiedAt { get; set; }
+}
