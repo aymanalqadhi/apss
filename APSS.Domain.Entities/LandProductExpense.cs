e8c4b753bf82c6823d701588be68b50a1f8346b5
wosama <almaktary.osama@gmail.com>
2022-04-28 07:47:27 +0300

    Add basic LandProductExpense entity

---
+++ APSS.Domain.Entities/LandProductExpense.cs
@@ -0,0 +1,23 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represt the expenses of the land's product
+/// </summary>
+public sealed class LandProductExpense : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the type of the expense
+    /// </summary>
+    public string Type { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the price of the expense
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Gets or sets the product that the expense was spent on
+    /// </summary>
+    public LandProduct SpentOn { get; set; } = null!;
+}
+
