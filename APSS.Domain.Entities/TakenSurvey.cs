480fbcf806f98acebe4d8850168c2997c2e423bf
wosama <almaktary.osama@gmail.com>
2022-04-28 07:49:26 +0300

    Add basic TakenSurvey entity

---
+++ APSS.Domain.Entities/TakenSurvey.cs
@@ -0,0 +1,22 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent the taken survey
+/// </summary>
+public sealed class TakenSurvey : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets who has taken the survey
+    /// </summary>
+    public User TakenBy { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the
+    /// </summary>
+    public Survey Survey { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the attempt date to take the survey
+    /// </summary>
+    public DateTime AttemptDate { get; set; }   
+}
