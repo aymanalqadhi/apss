3667efd4c1f387874de56ae12657cc7e5a6dd17d
wosama <almaktary.osama@gmail.com>
2022-04-28 07:45:50 +0300

    Add Confirmable base class

---
+++ APSS.Domain.Entities/Confirmable.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Domain.Entities;
+
+public abstract class Confirmable : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets wether it's confirmed or not
+    /// </summary>
+    public bool IsConfirmed { get; set; } = false;
+}
