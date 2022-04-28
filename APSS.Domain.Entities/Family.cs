60c11410792b02dca6288f410a6add17434ea520
wosama <almaktary.osama@gmail.com>
2022-04-28 07:46:06 +0300

    Add basic Family entity

---
+++ APSS.Domain.Entities/Family.cs
@@ -0,0 +1,22 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent a family
+/// </summary>
+public sealed class Family : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the family
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the living location of the family
+    /// </summary>
+    public string LivingLocation { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the user associated with the family
+    /// </summary>
+    public User AddedBy { get; set; } = null!;
+}
