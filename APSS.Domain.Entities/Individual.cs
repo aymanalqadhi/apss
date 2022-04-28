09736a2c62a14998e2ce70d63426788ee17b77b5
wosama <almaktary.osama@gmail.com>
2022-04-28 07:46:39 +0300

    Add basic Individual entity

---
+++ APSS.Domain.Entities/Individual.cs
@@ -0,0 +1,63 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent an individual of the system
+/// </summary>
+public sealed class Individual : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the individual
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the sex of the individual
+    /// </summary>
+    public string Sex { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the datebirth of the individual
+    /// </summary>
+    public DateTime DateBirth { get; set; }
+
+    /// <summary>
+    /// Gets or sets the national id of the individual
+    /// </summary>
+    public long NationalId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the phone number of the individual
+    /// </summary>
+    public long PhoneNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the job of the individual
+    /// </summary>
+    public string Job { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the social status of the individual
+    /// </summary>
+    public string SocialStatus { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the address of the individual
+    /// </summary>
+    public string Address { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the user associated with the individual
+    /// </summary>
+    public User AddedBy { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the skills of the individual
+    /// </summary>
+    public ICollection<Skill> Skills { get; set; } = new List<Skill>();
+
+    /// <summary>
+    /// Gets or sets the voluntary of the individual
+    /// </summary>
+    public ICollection<Voluntary> Voluntary { get; set; } = new List<Voluntary>();
+}
+
