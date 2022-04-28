3073cb6429734b29e3249237bc3e7cbeaea0e49f
wosama <almaktary.osama@gmail.com>
2022-04-28 07:50:07 +0300

    Add basic Season entity

---
+++ APSS.Domain.Entities/Season.cs
@@ -0,0 +1,33 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent a season
+/// </summary>
+public sealed class Season : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the season
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the begining month of the season
+    /// </summary>
+    public int BeginMonth { get; set; }
+
+    /// <summary>
+    /// Gets or sets the begining day of the season
+    /// </summary>
+    public int BeginDay { get; set; }
+
+    /// <summary>
+    /// Gets or sets the end month of the season
+    /// </summary>
+    public int EndMonth { get; set; }
+
+    /// <summary>
+    /// Gets or sets the end day of the season
+    /// </summary>
+    public int EndDay { get; set; }
+}
+
