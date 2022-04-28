5dfdff5deff3cc7da9e38781b925316fed200bfa
wosama <almaktary.osama@gmail.com>
2022-04-28 07:42:57 +0300

    Add basic AnimalProduct entity

---
+++ APSS.Domain.Entities/AnimalProduct.cs
@@ -0,0 +1,33 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent an animal product
+/// </summary>
+public sealed class AnimalProduct : Confirmable
+{
+    /// <summary>
+    /// Gets or sets the name of the animal's product
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the unit of the animal's product
+    /// </summary>
+    public string Unit { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the quantity of the animal's product
+    /// </summary>
+    public double Quantity { get; set; }
+
+    /// <summary>
+    /// Gets or sets the period taken to produce the animal's product 
+    /// </summary>
+    public string PeriodTaken { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the animal of the animal's product
+    /// </summary>
+    public AnimalGroup Producer { get; set; } = null!;
+}
+
