ce647d9ad84769afe9876fff99fc7621991ba700
wosama <almaktary.osama@gmail.com>
2022-04-28 07:42:37 +0300

    Add basic AnimalGroup entity

---
+++ APSS.Domain.Entities/AnimalGroup.cs
@@ -0,0 +1,34 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent an animal group
+/// </summary>
+public sealed class AnimalGroup : Confirmable
+{
+    /// <summary>
+    /// Gets or sets the name of the animal group
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the type of the animal group
+    /// </summary>
+    public string Type { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the quantity of the animal group
+    /// </summary>
+    public int Quantity { get; set; }
+
+    /// <summary>
+    /// Gets or sets the sex of the animal group
+    /// </summary>
+    public string Sex { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the owner of the animal group
+    /// </summary>
+    public User OwnedBy { get; set; } = null!;
+}
+
+
