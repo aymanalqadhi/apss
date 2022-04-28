d05339220003dd062fc6a831b7623ff072b4d9c9
wosama <almaktary.osama@gmail.com>
2022-04-28 07:48:09 +0300

    Add basic Voluntary entity

---
+++ APSS.Domain.Entities/Voluntary.cs
@@ -0,0 +1,22 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent the voluntary
+/// </summary>
+public sealed class Voluntary : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the voluntary
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the type of the voluntary
+    /// </summary>
+    public string Type { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the field of the voluntary
+    /// </summary>
+    public string Field { get; set; } = null!;
+}
