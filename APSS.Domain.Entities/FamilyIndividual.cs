e61fbecfc7357e58643960cd14d230d6b3033fe1
wosama <almaktary.osama@gmail.com>
2022-04-28 07:46:26 +0300

    Add basic FamilyIndividual entity

---
+++ APSS.Domain.Entities/FamilyIndividual.cs
@@ -0,0 +1,28 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent the association between family and individual
+/// </summary>
+public sealed class FamilyIndividual : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the inidviduals
+    /// </summary>
+    public Individual Individual { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the family
+    /// </summary>
+    public Family Family { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or stes the role of the individual in the family
+    /// </summary>
+    public string Role { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets wether the individual is a provider in the family or not
+    /// </summary>
+    public bool IsProvider { get; set; } = false;
+}
+
