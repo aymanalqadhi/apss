7347619e309a68436d7f22a30842d0af0cf05d17
wosama <almaktary.osama@gmail.com>
2022-04-28 07:47:47 +0300

    Add basic Skill entity

---
+++ APSS.Domain.Entities/Skill.cs
@@ -0,0 +1,22 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent skills of individual
+/// </summary>
+public sealed class Skill : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the skill
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the description of the skill
+    /// </summary>
+    public string Description { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the field of the skill
+    /// </summary>
+    public string Field { get; set; } = null!;
+}
