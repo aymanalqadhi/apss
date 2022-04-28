047c7e06b9e9d156905b1d7903b35b20d1cc4100
wosama <almaktary.osama@gmail.com>
2022-04-28 07:40:32 +0300

    Update basic user entity

--- APSS.Domain.Entities/User.cs
+++ APSS.Domain.Entities/User.cs
@@ -1,9 +1,58 @@
 ﻿namespace APSS.Domain.Entities;
 
+/// <summary>
+/// A class to represent a user of the system
+/// </summary>
 public sealed class User : AuditableEntity
 {
     /// <summary>
-    /// Gets or sets the number of the users
+    /// Gets or stes the name of the user 
     /// </summary>
-    public string UserNumber { get; set; } = null!;
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the password hash of the user 
+    /// </summary>
+    public string PasswordHash { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the password salt of the user 
+    /// </summary>
+    public string PasswordSalt { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the national id of the user 
+    /// </summary>
+    public long NationalId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the phone number of the user 
+    /// </summary>
+    public long PhoneNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the social status of the user 
+    /// </summary>
+    public string SocialStatus { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the job of the user 
+    /// </summary>
+    public string Job { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the access level of the user 
+    /// </summary>
+    public int AccessLevel { get; set; }
+
+    /// <summary>
+    /// Gets or sets wether the user is a supervisor or not
+    /// </summary>
+    public bool IsSupervisor { get; set; } = false;
+
+    /// <summary>
+    /// Gets ot sets the supervisor
+    /// </summary>
+    public User SupervisedBy { get; set; } = null!;
 }
+
