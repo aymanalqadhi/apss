244b5a89f849f9f9bd565aca0774e1f77f9425af
wosama <almaktary.osama@gmail.com>
2022-04-28 07:48:57 +0300

    Add basic PermissionInheritance entity

---
+++ APSS.Domain.Entities/PermissionInheritance.cs
@@ -0,0 +1,22 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent permission inheritance
+/// </summary>
+public sealed class PermissionInheritance : AuditableEntity
+{
+    /// <summary>
+    /// Gets or set the user whose permissions inherited from
+    /// </summary>
+    public User From { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or set the user who inherited the permissions
+    /// </summary>
+    public User To { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the time validity of the permission inherited
+    /// </summary>
+    public DateTime ValidUntil { get; set; }
+}
