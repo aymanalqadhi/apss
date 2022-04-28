a19a466520bb2516729bcd164799b7a4c4647964
wosama <almaktary.osama@gmail.com>
2022-04-28 07:48:18 +0300

    Add basic Log entity

---
+++ APSS.Domain.Entities/Log.cs
@@ -0,0 +1,23 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent a log
+/// </summary>
+public sealed class Log : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the message of the log
+    /// </summary>
+    public string Message { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the timestamp of the log
+    /// </summary>
+    public DateTime TimeStamp { get; set; }
+
+    /// <summary>
+    /// Gets or sets the tags of the log
+    /// </summary>
+    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+}
+
