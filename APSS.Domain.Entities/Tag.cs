f5a1efe313e566eeacea0aa83cbbf7e4e26d2042
wosama <almaktary.osama@gmail.com>
2022-04-28 07:48:27 +0300

    Add basic Tag entity

---
+++ APSS.Domain.Entities/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Domain.Entities;
+
+public class Tag : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the tag number
+    /// </summary>
+    public int Number { get; set; }
+}
