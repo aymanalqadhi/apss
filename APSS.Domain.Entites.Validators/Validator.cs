2b369dd592d579b0f070ef02a73c4e9cab78564f
wosama <almaktary.osama@gmail.com>
2022-05-14 22:08:33 +0300

    Add base Validator class

---
+++ APSS.Domain.Entites.Validators/Validator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// Base validator type
+/// </summary>
+/// <typeparam name="T">The type to be validated</typeparam>
+public class Validator<T> : AbstractValidator<T>
+{
+}
