590fe3848b63af28a3fff44867a17eb063a7b3cb
wosama <almaktary.osama@gmail.com>
2022-05-24 18:57:13 +0300

    Update Survey entity

--- APSS.Domain.Entities/Survey.cs
+++ APSS.Domain.Entities/Survey.cs
@@ -16,21 +16,6 @@ public sealed class Survey : AuditableEntity
     public DateTime ExpirationDate { get; set; }
 
     /// <summary>
-    /// Gets or sets the creation date of the survey
-    /// </summary>
-    public DateTime CreationDate { get; set; }
-
-    /// <summary>
-    /// Gets or sets the end month of the survey
-    /// </summary>
-    public int EndMonth { get; set; }
-
-    /// <summary>
-    /// Gets or sets the end day of the survey
-    /// </summary>
-    public int EndDay { get; set; }
-
-    /// <summary>
     /// Gets or sets the user who created the survey
     /// </summary>
     public User CreatedBy { get; set; } = null!;
