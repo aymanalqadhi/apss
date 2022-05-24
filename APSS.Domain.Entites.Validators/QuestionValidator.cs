24ac894ed358752d1eb2dae082073acf26ca3e20
wosama <almaktary.osama@gmail.com>
2022-05-24 19:09:10 +0300

    Add Question validator

---
+++ APSS.Domain.Entites.Validators/QuestionValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.Question"/>
+/// </summary>
+public sealed class QuestionValidator : Validator<Entities.Question>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public QuestionValidator()
+    {
+        RuleFor(q => q.Index)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("index is required and cannot be less than 0");
+
+    }
+}
