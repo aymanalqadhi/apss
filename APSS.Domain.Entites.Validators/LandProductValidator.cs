f757805e26131c74593af7d25d8303da823bd39e
wosama <almaktary.osama@gmail.com>
2022-05-24 19:08:49 +0300

    Add LandProduct validator

---
+++ APSS.Domain.Entites.Validators/LandProductValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.LandProduct"/>
+/// </summary>
+public sealed class LandProductValidator : Validator<Entities.LandProduct>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public LandProductValidator()
+    {
+        RuleFor(l => l.CropName)
+            .NotEmpty()
+            .WithMessage("name is required");
+
+
+        RuleFor(l => l.Quantity)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("quantity must be higher or equal to 0");
+
+    }
+}
