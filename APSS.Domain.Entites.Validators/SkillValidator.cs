a06d9d4202b449d822db3a34461d2363821e93f7
wosama <almaktary.osama@gmail.com>
2022-05-24 19:10:14 +0300

    Add Skill validator

---
+++ APSS.Domain.Entites.Validators/SkillValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.Skill"/>
+/// </summary>
+public sealed class SkillValidator : Validator<Entities.Skill>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public SkillValidator()
+    {
+        RuleFor(s => s.Name)
+            .NotEmpty()
+            .WithMessage("name is required");
+    }
+}
