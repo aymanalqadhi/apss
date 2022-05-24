1e76e23a64045991dd641e4a471342740d4c0c6a
wosama <almaktary.osama@gmail.com>
2022-05-24 19:09:59 +0300

    Add Season validator

---
+++ APSS.Domain.Entites.Validators/SeasonValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.Season"/>
+/// </summary>
+public sealed class SeasonValidator : Validator<Entities.Season>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public SeasonValidator()
+    {
+        RuleFor(s => s.Name)
+            .NotEmpty()
+            .WithMessage("name is required");
+
+        RuleFor(s => s.BeginDay)
+            .GreaterThan(0)
+            .LessThan(32)
+            .WithMessage("begin day cannot be less than 1 or greater than 31");
+
+        RuleFor(s => s.EndDay)
+            .GreaterThan(0)
+            .LessThan(32)
+            .WithMessage("end day cannot be less than 1 or greater than 31");
+
+        RuleFor(s => s.BeginMonth)
+            .GreaterThan(0)
+            .LessThan(13)
+            .WithMessage("begin month cannot be less than 1 or greater than 12");
+
+        RuleFor(s => s.EndMonth)
+            .GreaterThan(0)
+            .LessThan(13)
+            .WithMessage("begin day cannot be less than 1 or greater than 12");
+    }
+}
