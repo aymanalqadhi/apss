8acce9c77b479b177077e90a3ec6124cc383bd9c
wosama <almaktary.osama@gmail.com>
2022-05-24 19:08:59 +0300

    Add Land validator

---
+++ APSS.Domain.Entites.Validators/LandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.Land"/>
+/// </summary>
+public sealed class LandValidator : Validator<Entities.Land>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public LandValidator()
+    {
+        RuleFor(l => l.Name)
+            .NotEmpty()
+            .WithMessage("name is required");
+
+        RuleFor(l => l.OwnedBy.AccessLevel)
+            .Equal(Entities.AccessLevel.Farmer)
+            .WithMessage("a land must be owned by a farmer");
+
+    }
+}
