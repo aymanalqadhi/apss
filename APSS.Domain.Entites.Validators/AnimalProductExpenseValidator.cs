0c81b2587779148dd640163abb0e7690aff27f48
wosama <almaktary.osama@gmail.com>
2022-05-24 19:07:28 +0300

    Add AnimalProductExpense validator

---
+++ APSS.Domain.Entites.Validators/AnimalProductExpenseValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.AnimalProductExpens"/>
+/// </summary>
+public sealed class AnimalProductExpenseValidator : Validator<Entities.AnimalProductExpens>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public AnimalProductExpenseValidator()
+    {
+        RuleFor(a => a.Price)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("price must be higher or equal to 0");
+
+    }
+}
