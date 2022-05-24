10857237e6b3d15e1f62661ec3533657116cae0c
wosama <almaktary.osama@gmail.com>
2022-05-24 19:10:50 +0300

    Add User validator

---
+++ APSS.Domain.Entites.Validators/UserValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.User"/>
+/// </summary>
+public sealed class UserValidator : Validator<Entities.User>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public UserValidator()
+    {
+        RuleFor(user => user.Name)
+            .NotEmpty()
+            .WithMessage("name is required");
+
+        RuleFor(user => user.NationalId)
+            .NotEmpty()
+            .WithMessage("national ID is required");
+
+        RuleFor(user => user.IsSupervisor)
+            .Equal(false)
+            .When(user => user.AccessLevel == Entities.AccessLevel.Farmer);
+
+
+    }
+}
