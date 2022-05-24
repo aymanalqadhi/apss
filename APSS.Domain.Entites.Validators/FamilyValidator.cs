1fc60b131adb4a4c8fe7641a4aac546267bc607d
wosama <almaktary.osama@gmail.com>
2022-05-24 19:07:58 +0300

    Add Family validator

---
+++ APSS.Domain.Entites.Validators/FamilyValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.Family"/>
+/// </summary>
+public sealed class FamilyValidator : Validator<Entities.Family>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public FamilyValidator()
+    {
+        RuleFor(f => f.Name)
+            .NotEmpty()
+            .WithMessage("name is required");
+
+    }
+}
