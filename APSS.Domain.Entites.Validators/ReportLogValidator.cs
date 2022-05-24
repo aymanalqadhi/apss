492dd7bdc0b5552b39ced9c2a3ca02856e446017
wosama <almaktary.osama@gmail.com>
2022-05-24 19:09:25 +0300

    Add ReportLog validator

---
+++ APSS.Domain.Entites.Validators/ReportLogValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.ReportLog"/>
+/// </summary>
+public sealed class ReportLogValidator : Validator<Entities.ReportLog>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public ReportLogValidator()
+    {
+        RuleFor(r => r.Name)
+            .NotEmpty()
+            .WithMessage("name is required");
+
+    }
+}
