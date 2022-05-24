12d55a092c1116afd32eb4ef1e238309394ff035
wosama <almaktary.osama@gmail.com>
2022-05-24 19:08:35 +0300

    Add LandProductExpense validator

---
+++ APSS.Domain.Entites.Validators/LandProductExpenseValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.LandProductExpense"/>
+/// </summary>
+public sealed class LandProductExpenseValidator : Validator<Entities.LandProductExpense>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public LandProductExpenseValidator()
+    {
+        RuleFor(l => l.Price)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("price must be higher than or equal to 0");
+
+    }
+}
