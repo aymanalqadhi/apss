93a03cd97d95a6d2912adfd3978a9a98d25abba7
wosama <almaktary.osama@gmail.com>
2022-05-24 19:11:03 +0300

    Add Voluntary validator

---
+++ APSS.Domain.Entites.Validators/VoluntaryValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.Voluntary"/>
+/// </summary>
+public sealed class VoluntaryValidator : Validator<Entities.Voluntary>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public VoluntaryValidator()
+    {
+        RuleFor(v => v.Name)
+            .NotEmpty()
+            .WithMessage("name is required");
+
+        RuleFor(v => v.Type)
+            .NotEmpty()
+            .WithMessage("type is required");
+
+        RuleFor(v => v.Field)
+           .NotEmpty()
+           .WithMessage("field is required");
+    }
+}
