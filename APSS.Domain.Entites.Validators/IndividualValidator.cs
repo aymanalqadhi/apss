b6d6e06a9645e39828a434d67d11a4fdbacfe8d7
wosama <almaktary.osama@gmail.com>
2022-05-24 19:08:09 +0300

    Add Individual validator

---
+++ APSS.Domain.Entites.Validators/IndividualValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.Individual"/>
+/// </summary>
+public sealed class IndividualValidator : Validator<Entities.Individual>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public IndividualValidator()
+    {
+        RuleFor(i => i.Name)
+            .NotEmpty()
+            .WithMessage("name is required");
+
+    }
+}
