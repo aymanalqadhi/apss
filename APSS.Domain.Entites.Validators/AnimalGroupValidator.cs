0bb835fb965b47e17af589a1f2b3a03991d55466
wosama <almaktary.osama@gmail.com>
2022-05-24 18:59:26 +0300

    Add AnimalGroup validator

---
+++ APSS.Domain.Entites.Validators/AnimalGroupValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entites.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Entities.AnimalGroup"/>
+/// </summary>
+public sealed class AnimalGroupValidator : Validator<Entities.AnimalGroup>
+{
+    /// <summary>
+    /// Default constuctor
+    /// </summary>
+    public AnimalGroupValidator()
+    {
+        RuleFor(g => g.Name)
+            .NotEmpty()
+            .WithMessage("animal group name cannot be empty");
+
+        RuleFor(g => g.Type)
+            .NotEmpty()
+            .WithMessage("animal group type cannot be empty");
+
+        RuleFor(g => g.Quantity)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("animal group quantity must be greater then or equal to 0");
+
+        RuleFor(g => g.OwnedBy.AccessLevel)
+            .Equal(Entities.AccessLevel.Farmer);
+
+    }
+}
+
