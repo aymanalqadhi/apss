07545252587d225329775b919d00000396a0911f
wosama <almaktary.osama@gmail.com>
2022-05-27 00:32:12 +0300

    Add TextQuestionAnswer validator

---
+++ APSS.Domain.Entites.Validators/TextQuestionAnswerValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="TextQuestionAnswer"/>
+/// </summary>
+public sealed class TextQuestionValidator : Validator<TextQuestionAnswer>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public TextQuestionValidator()
+    {
+        RuleFor(t => t.Answer)
+            .Must(a => !string.IsNullOrEmpty(a?.Trim()))
+            .When(a => a.Question.IsRequired)
+            .WithMessage("a text answer is required");
+    }
+}
