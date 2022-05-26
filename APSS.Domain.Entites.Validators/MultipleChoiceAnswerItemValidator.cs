9c8f7f9e4408ae1e5c20ea0a4d9c4adfcdf08649
wosama <almaktary.osama@gmail.com>
2022-05-27 00:32:48 +0300

    Add MultipleChoiceAnswerItem validator

---
+++ APSS.Domain.Entites.Validators/MultipleChoiceAnswerItemValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="MultipleChoiceAnswerItem"/>
+/// </summary>
+public sealed class MultipleChoiceAnswerItemValidator : Validator<MultipleChoiceAnswerItem>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public MultipleChoiceAnswerItemValidator()
+    {
+        RuleFor(m => m.Value)
+            .NotEmpty()
+            .WithMessage("multiple choice answer item value cannot be empty");
+    }
+}
