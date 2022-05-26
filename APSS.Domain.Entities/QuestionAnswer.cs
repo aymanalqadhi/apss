47f4e686d191a8fc5c8b253cc15900b486c2a5ef
wosama <almaktary.osama@gmail.com>
2022-05-26 23:57:01 +0300

    Add abstract QuestionAnswer entity

---
+++ APSS.Domain.Entities/QuestionAnswer.cs
@@ -0,0 +1,12 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// An abstract class to represent a question answer
+/// </summary>
+public abstract class QuestionAnswer
+{
+    /// <summary>
+    /// Gets or sets the Question
+    /// </summary>
+    public Question Question { get; set; } = null!;
+}
