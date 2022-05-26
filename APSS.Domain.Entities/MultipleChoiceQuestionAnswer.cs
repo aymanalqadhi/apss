839718654505b5be4ccd4d547347d1ccc3894a0b
wosama <almaktary.osama@gmail.com>
2022-05-26 23:58:03 +0300

    Add MultipleChoiceQuestionAnswer entity

---
+++ APSS.Domain.Entities/MultipleChoiceQuestionAnswer.cs
@@ -0,0 +1,12 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent 
+/// </summary>
+public sealed class MultipleChoiceQuestionAnswer : QuestionAnswer
+{
+    /// <summary>
+    /// Gets or sets the answers of the multiple choice question
+    /// </summary>
+    public ICollection<MultipleChoiceAnswerItem> Answers { get; set; } = new List<MultipleChoiceAnswerItem>();
+}
