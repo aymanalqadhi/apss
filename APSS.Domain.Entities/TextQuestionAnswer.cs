bbf784b59cab6d9da39144bd9b7550cf45bc2592
wosama <almaktary.osama@gmail.com>
2022-05-26 23:58:20 +0300

    Add TextQuestionAnswer entity

---
+++ APSS.Domain.Entities/TextQuestionAnswer.cs
@@ -0,0 +1,12 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent a text question answer
+/// </summary>
+public sealed class TextQuestionAnswer : QuestionAnswer
+{
+    /// <summary>
+    /// Gets or sets the answer of the text question
+    /// </summary>
+    public TextQuestion Answer { get; set; } = null!;
+}
