6435afa912392034e60a08a66d24b7feebdb6496
aymanalqadhi <alqd@protonmail.com>
2022-05-26 23:48:38 +0300

    Implement IDatabaseTransaction interface

---
+++ APSS.Infrastructure.Repositores.EntityFramework/DatabaseTransaction.cs
@@ -0,0 +1,32 @@
+﻿using APSS.Domain.Repositories;
+
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace APSS.Infrastructure.Repositores.EntityFramework;
+
+public class DatabaseTransaction : IDatabaseTransaction
+{
+    private readonly IDbContextTransaction _tx;
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="tx">Entity framework transaction object</param>
+    public DatabaseTransaction(IDbContextTransaction tx)
+        => _tx = tx;
+
+    /// <inheritdoc/>
+    public Task CommitAsync(CancellationToken cancellationToken = default)
+        => _tx.CommitAsync(cancellationToken);
+
+    /// <inheritdoc/>
+    public Task RollbackAsync(CancellationToken cancellationToken = default)
+        => _tx.RollbackAsync(cancellationToken);
+
+    /// <inheritdoc/>
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return _tx.DisposeAsync();
+    }
+}
