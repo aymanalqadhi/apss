c23ee2fe0fbe566ef893512c28ae2922de3f7aa1
wosama <almaktary.osama@gmail.com>
2022-05-27 00:32:33 +0300

    Add LogicQuestionAnswer validator

---
+++ APSS.Domain.Entites.Validators/LogicalQuestionAnswerValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="TextQuestionAnswer"/>
+/// </summary>
+public sealed class LogicalQuestionAnswerValidator : Validator<LogicalQuestionAnswer>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public LogicalQuestionAnswerValidator()
+    {
+        RuleFor(t => t.Answer)
+            .NotNull()
+            .When(a => a.Question.IsRequired)
+            .WithMessage("an answer is required");
+    }
+}
