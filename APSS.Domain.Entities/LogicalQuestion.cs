e5dcdc9408667ef144ca5ec0b8c0d958a43795e3
wosama <almaktary.osama@gmail.com>
2022-05-27 00:22:17 +0300

    Remove uneeded answer field

--- APSS.Domain.Entities/LogicalQuestion.cs
+++ APSS.Domain.Entities/LogicalQuestion.cs
@@ -2,8 +2,4 @@
 
 public sealed class LogicalQuestion : Question
 {
-    /// <summary>
-    /// Gets or sets whther this question was answered by yes/true or no/false
-    /// </summary>
-    public bool Answer { get; set; }
 }
--- APSS.Domain.Entities/TextQuestion.cs
+++ APSS.Domain.Entities/TextQuestion.cs
@@ -2,8 +2,4 @@
 
 public sealed class TextQuestion : Question
 {
-    /// <summary>
-    /// Gets or sets the plain text of the answer
-    /// </summary>
-    public string Answer { get; set; } = null!;
 }
