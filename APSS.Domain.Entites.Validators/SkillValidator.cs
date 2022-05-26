28b95795e2232a39fd6b8f46d6ff333ccd9f3807
aymanalqadhi <alqd@protonmail.com>
2022-05-26 21:15:16 +0300

    Add a using statement to the entites project

--- APSS.Domain.Entites.Validators/SkillValidator.cs
+++ APSS.Domain.Entites.Validators/SkillValidator.cs
@@ -1,11 +1,13 @@
-﻿using FluentValidation;
+﻿using APSS.Domain.Entities;
+
+using FluentValidation;
 
 namespace APSS.Domain.Entites.Validators;
 
 /// <summary>
-/// A validator for the entity <see cref="Entities.Skill"/>
+/// A validator for the entity <see cref="Skill"/>
 /// </summary>
-public sealed class SkillValidator : Validator<Entities.Skill>
+public sealed class SkillValidator : Validator<Skill>
 {
     /// <summary>
     /// Default constructor
