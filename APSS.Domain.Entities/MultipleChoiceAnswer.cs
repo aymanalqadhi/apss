13d859390fb0047a2dc41f38e4d5488bce6d6961
aymanalqadhi <alqd@protonmail.com>
2022-05-26 20:51:29 +0300

    Add multiple choice question type

---
+++ APSS.Domain.Entities/MultipleChoiceAnswer.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Domain.Entities;
+
+public sealed class MultipleChoiceAnswer
+{
+    /// <summary>
+    /// Gets or sets the value of the answer
+    /// </summary>
+    public string Value { get; set; } = null!;
+}
---
+++ APSS.Domain.Entities/MultipleChoiceQuestion.cs
@@ -0,0 +1,15 @@
+﻿namespace APSS.Domain.Entities;
+
+public sealed class MultipleChoiceQuestion : Question
+{
+    /// <summary>
+    /// Gets or sets the collection of possible answers to this question
+    /// </summary>
+    public ICollection<MultipleChoiceAnswer> Answers { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets whether multiple answers can be selected at a time
+    /// </summary>
+    public bool CanMultiSelect { get; set; } = false;
+}
+    
