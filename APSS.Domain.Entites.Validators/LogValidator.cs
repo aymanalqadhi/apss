b911a1cb17974ad65cb216b5a17e982afca848b2
wosama <almaktary.osama@gmail.com>
2022-05-26 23:59:35 +0300

    Add Log validator

---
+++ APSS.Domain.Entites.Validators/LogValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Log"/>
+/// </summary>
+public sealed class LogValidator : Validator<Log>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public LogValidator()
+    {
+        RuleFor(l => l.Message)
+            .NotEmpty()
+            .WithMessage("log message cannot be empty");
+    }
+}
