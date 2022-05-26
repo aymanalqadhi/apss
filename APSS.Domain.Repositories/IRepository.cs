78b4d154a1f9eaef573f1e105d0cecf4c9d90015
aymanalqadhi <alqd@protonmail.com>
2022-05-26 22:14:34 +0300

    Add an interface for a general-purpose repository

---
+++ APSS.Domain.Repositories/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using APSS.Domain.Entities;
+
+namespace APSS.Domain.Repositories;
+
+public interface IRepository<T> where T : AuditableEntity
+{
+    /// <summary>
+    /// Creates a query builder for <see cref="T"/>
+    /// </summary>
+    /// <returns>The query builder object</returns>
+    IQueryBuilder<T> Query();
+
+    /// <summary>
+    /// Adds an item to the repository
+    /// </summary>
+    /// <param name="entity">The item to add</param>
+    void Add(T entity);
+
+    /// <summary>
+    /// Updates an item in the repository
+    /// </summary>
+    /// <param name="entity">The updated item</param>
+    void Update(T entity);
+
+    /// <summary>
+    /// Deletes an item from the repositry
+    /// </summary>
+    /// <param name="entity">The item to delete</param>
+    void Delete(T entity);
+}
