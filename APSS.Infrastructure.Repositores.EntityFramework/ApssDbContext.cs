9f577d67f4c218e948b3aa88d34478be85ad60f4
aymanalqadhi <alqd@protonmail.com>
2022-05-26 22:44:47 +0300

    Add a basic DbContext class

---
+++ APSS.Infrastructure.Repositores.EntityFramework/ApssDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APSS.Infrastructure.Repositores.EntityFramework;
+
+public sealed class ApssDbContext : DbContext
+{
+    #region Ctors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="options">Options to be used to configure the database context</param>
+    public ApssDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    #endregion
+}
