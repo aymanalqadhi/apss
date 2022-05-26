d85ee6b68b14ac5f172fb1397d39bd44200f2119
aymanalqadhi <alqd@protonmail.com>
2022-05-26 20:31:05 +0300

    Merge animal and land products expenses classes into one class

--- APSS.Domain.Entities/ProductExpense.cs
+++ APSS.Domain.Entities/ProductExpense.cs
@@ -1,9 +1,9 @@
 ﻿namespace APSS.Domain.Entities;
 
 /// <summary>
-/// A class to represt the expenses of the land's product
+/// A class to represt the expenses of the anamil's product
 /// </summary>
-public sealed class LandProductExpense : AuditableEntity
+public sealed class ProductExpense : AuditableEntity
 {
     /// <summary>
     /// Gets or sets the type of the expense
@@ -16,8 +16,7 @@ public sealed class LandProductExpense : AuditableEntity
     public decimal Price { get; set; }
 
     /// <summary>
-    /// Gets or sets the product that the expense was spent on
+    /// Gets or sets the item that the expense was spent on
     /// </summary>
-    public LandProduct SpentOn { get; set; } = null!;
+    public Ownable SpentOn { get; set; } = null!;
 }
-
