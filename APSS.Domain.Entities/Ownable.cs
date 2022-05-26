6f8e72ee9ac2ba8e310d9897db24d521b79f4e35
aymanalqadhi <alqd@protonmail.com>
2022-05-27 00:13:40 +0300

    Remove uneeded Expenses navigation property

--- APSS.Domain.Entities/Ownable.cs
+++ APSS.Domain.Entities/Ownable.cs
@@ -11,9 +11,4 @@ public abstract class Ownable : Confirmable
     /// Gets or sets the owner of the ownable item
     /// </summary>
     public User OwnedBy { get; set; } = null!;
-
-    /// <summary>
-    /// Gets or sets the collection of expenses spent on this item
-    /// </summary>
-    public ICollection<ProductExpense> Expenses { get; set; } = new List<ProductExpense>();
 }
