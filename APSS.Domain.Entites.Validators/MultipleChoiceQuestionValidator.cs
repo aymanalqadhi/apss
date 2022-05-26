ac610c6fb7cee863483441ca8a528f182ffca3d8
wosama <almaktary.osama@gmail.com>
2022-05-27 00:33:28 +0300

    Add MultipleChoiceQuestion validator

---
+++ APSS.Domain.Entites.Validators/MultipleChoiceQuestionValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="MultipleChoiceQuestion"/>
+/// </summary>
+public sealed class MultipleChoiceQuestionValidator : Validator<MultipleChoiceQuestion>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public MultipleChoiceQuestionValidator()
+    {
+        RuleFor(q => q.Answers.Count)
+            .GreaterThanOrEqualTo(2)
+            .WithMessage("multiple choice questions must have at least two answers");
+
+    }
+}
