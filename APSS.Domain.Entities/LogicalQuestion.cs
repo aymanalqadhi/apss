bf7658e8519aea9f87a238c635e8cfbe54f178dd
aymanalqadhi <alqd@protonmail.com>
2022-05-26 20:51:44 +0300

    Add logical (true, false) question

---
+++ APSS.Domain.Entities/LogicalQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Domain.Entities;
+
+public sealed class LogicalQuestion : Question
+{
+    /// <summary>
+    /// Gets or sets whther this question was answered by yes/true or no/false
+    /// </summary>
+    public bool Answer { get; set; }
+}
