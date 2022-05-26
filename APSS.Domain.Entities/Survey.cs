7375ba88767bb3915a4476d08fca34577503d4c9
aymanalqadhi <alqd@protonmail.com>
2022-05-26 20:52:13 +0300

    Add a navigation property to access survey questions

--- APSS.Domain.Entities/Survey.cs
+++ APSS.Domain.Entities/Survey.cs
@@ -21,6 +21,11 @@ public sealed class Survey : AuditableEntity
     public User CreatedBy { get; set; } = null!;
 
     /// <summary>
+    /// Gets or sets the collection of questions associated with this survey
+    /// </summary>
+    public ICollection<Question> Questions { get; set; } = new List<Question>();
+
+    /// <summary>
     /// Gets or sets the collection of entries for this surveys
     /// </summary>
     public ICollection<SurveyEntry> Entries { get; set; } = new List<SurveyEntry>();
