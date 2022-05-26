3ecc7713748db02715946164290ce953cfaa241c
wosama <almaktary.osama@gmail.com>
2022-05-26 22:19:34 +0300

    Remove unneeded rules for season

--- APSS.Domain.Entites.Validators/SeasonValidator.cs
+++ APSS.Domain.Entites.Validators/SeasonValidator.cs
@@ -15,25 +15,5 @@ public sealed class SeasonValidator : Validator<Season>
         RuleFor(s => s.Name)
             .NotEmpty()
             .WithMessage("name is required");
-
-        RuleFor(s => s.BeginDay)
-            .GreaterThan(0)
-            .LessThan(32)
-            .WithMessage("begin day cannot be less than 1 or greater than 31");
-
-        RuleFor(s => s.EndDay)
-            .GreaterThan(0)
-            .LessThan(32)
-            .WithMessage("end day cannot be less than 1 or greater than 31");
-
-        RuleFor(s => s.BeginMonth)
-            .GreaterThan(0)
-            .LessThan(13)
-            .WithMessage("begin month cannot be less than 1 or greater than 12");
-
-        RuleFor(s => s.EndMonth)
-            .GreaterThan(0)
-            .LessThan(13)
-            .WithMessage("begin day cannot be less than 1 or greater than 12");
     }
 }
