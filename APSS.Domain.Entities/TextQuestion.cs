54010ddbeb651ebe794b46abe032221dbc4ab277
aymanalqadhi <alqd@protonmail.com>
2022-05-26 20:51:52 +0300

    Add a text question type

---
+++ APSS.Domain.Entities/TextQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Domain.Entities;
+
+public sealed class TextQuestion : Question
+{
+    /// <summary>
+    /// Gets or sets the plain text of the answer
+    /// </summary>
+    public string Answer { get; set; } = null!;
+}
