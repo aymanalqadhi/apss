b5c7c5cff4eecc2a36b7b005fe5bfc9d00536eea
aymanalqadhi <alqd@protonmail.com>
2022-05-27 03:31:45 +0300

    Test LogValidator class

---
+++ APSS.Domain.Entities.Validators.Test/LogValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using APSS.Tests.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APSS.Domain.Entities.Validators.Test;
+
+[TestClass]
+public class LogValidatorTests
+{
+    #region Private fields
+    
+    private readonly LogValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateMessageShouldSucceed()
+    {
+        var log = new Log
+        {
+            Message = RandomGenerator.NextString(0xff)
+        };
+
+        Assert.IsTrue(_validator.Validate(log).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateMessageShouldFail()
+    {
+        var log = new Log { Message = "" };
+
+        Assert.IsFalse(_validator.Validate(log).IsValid);
+    }
+
+    #endregion
+}
