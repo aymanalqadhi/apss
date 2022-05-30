e67417936fce18f9db1e999b98c6677b340b1ac1
aymanalqadhi <alqd@protonmail.com>
2022-05-30 19:26:30 +0300

    Create a testing database context that uses memory as a backing store

---
+++ APSS.Tests.Infrastructure.Repositories.EntityFramework/TestDbContext.cs
@@ -0,0 +1,20 @@
+﻿using APSS.Infrastructure.Repositores.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace APSS.Tests.Infrastructure.Repositories.EntityFramework;
+
+public static class TestDbContext
+{
+    /// <summary>
+    /// Creates an in-memory database context
+    /// </summary>
+    /// <returns></returns>
+    public static ApssDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<ApssDbContext>()
+            .UseInMemoryDatabase("test_apss_db")
+            .Options;
+
+        return new ApssDbContext(options);
+    }
+}
