59b0e0671356a35279d4d992e416859eb214ba2f
wosama <almaktary.osama@gmail.com>
2022-05-30 23:39:41 +0300

    Test TextQuestionAnswerValidator class

---
+++ APSS.Domain.Entities.Validators.Test/TextQuestionAnswerValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentValidation.TestHelper;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class TextQuestionAnswerValidatorTests
+{
+    #region Private fields
+
+    private readonly TextQuestionAnswerValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateTextQuestionAnswerShouldSucceed()
+    {
+        var textQuestion = new TextQuestionAnswer
+        {
+            Question = new TextQuestion { IsRequired = true},
+            Answer = RandomGenerator.NextString(0xff)
+        };
+
+        Assert.IsTrue(_validator.Validate(textQuestion).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateTextQuestionAnswerShouldFail()
+    {
+        var textQuestion = new TextQuestionAnswer
+        {
+            Question = new LogicalQuestion { IsRequired = true},
+            Answer = ""
+        };
+
+        var result = _validator.TestValidate(textQuestion);
+
+        result.ShouldHaveValidationErrorFor(a => a.Question);
+        result.ShouldHaveValidationErrorFor(a => a.Answer);
+    }
+
+    #endregion
+}
