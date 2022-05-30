286d7fb2d0387d20287f9c244ae684e155554967
wosama <almaktary.osama@gmail.com>
2022-05-30 21:16:42 +0300

    Test IndividualValidator class

---
+++ APSS.Domain.Entities.Validators.Test/IndividualValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentValidation.TestHelper;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class IndividualValidatorTests
+{
+    #region Private fields
+
+    private readonly IndividualValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateIndividualShouldSucceed()
+    {
+        var individual = new Individual
+        {
+            Name = RandomGenerator.NextString(0xff),
+            AddedBy = new User { AccessLevel = AccessLevel.Group}
+        };
+
+        Assert.IsTrue(_validator.Validate(individual).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateIndividualShouldFail()
+    {
+        var individual = new Individual
+        {
+            Name = "",
+            AddedBy = new User { AccessLevel = AccessLevel.Farmer }
+        };
+
+        var result = _validator.TestValidate(individual);
+
+        result.ShouldHaveValidationErrorFor(a => a.Name);
+        result.ShouldHaveValidationErrorFor(a => a.AddedBy.AccessLevel);
+    }
+
+    #endregion
+}
