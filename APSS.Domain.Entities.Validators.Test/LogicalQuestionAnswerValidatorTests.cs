64683c25d50a24694a5f3601c9310ddec009beb3
wosama <almaktary.osama@gmail.com>
2022-05-30 21:18:09 +0300

    Test LogicalQuestionAnswerValidator class

---
+++ APSS.Domain.Entities.Validators.Test/LogicalQuestionAnswerValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentValidation.TestHelper;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class LogicalQuestionAnswerValidatorTests
+{
+    #region Private fields
+
+    private readonly LogicalQuestionAnswerValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateLogicalQuestionAnswerShouldSucceed()
+    {
+        var logicalQuestionAnswer = new LogicalQuestionAnswer
+        {
+            Question = new LogicalQuestion { IsRequired = true},
+            Answer = true
+        };
+
+        Assert.IsTrue(_validator.Validate(logicalQuestionAnswer).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateLogicalQuestionAnswerShouldFail()
+    {
+        var logicalQuestionAnswer = new LogicalQuestionAnswer
+        {
+            Question = new MultipleChoiceQuestion { IsRequired = true},
+            Answer = null
+        };
+
+        var result = _validator.TestValidate(logicalQuestionAnswer);
+
+        result.ShouldHaveValidationErrorFor(a => a.Question);
+        result.ShouldHaveValidationErrorFor(a => a.Answer);
+    }
+
+    #endregion
+}
