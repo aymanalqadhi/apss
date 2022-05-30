10963e62874ffc9953e26ff41771c257fdcc90da
wosama <almaktary.osama@gmail.com>
2022-05-30 23:39:02 +0300

    Test SeasonValidator class

---
+++ APSS.Domain.Entities.Validators.Test/SeasonValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentValidation.TestHelper;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class SeasonValidatorTests
+{
+    #region Private fields
+    
+    private readonly SeasonValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateSeasonShouldSucceed()
+    {
+        var season = new Season
+        {
+            Name = RandomGenerator.NextString(0xff),
+        };
+
+        Assert.IsTrue(_validator.Validate(season).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateSeasonShouldFail()
+    {
+        var season = new Season 
+        {
+            Name = "",
+        };
+
+        var result = _validator.TestValidate(season);
+
+        result.ShouldHaveValidationErrorFor(l => l.Name);
+    }
+
+    #endregion
+}
