27f529a75f5db9ce6b547121f334b4e2652cdb56
wosama <almaktary.osama@gmail.com>
2022-05-30 23:38:29 +0300

    Test MultipleChoiceQuestionValidator class

---
+++ APSS.Domain.Entities.Validators.Test/MultipleChoiceQuestionValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentValidation.TestHelper;
+using System.Linq;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class MultipleChoiceQuestionValidatorTests
+{
+    #region Private fields
+    
+    private readonly MultipleChoiceQuestionValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateMultipleChoiceQuestionShouldSucceed()
+    {
+        
+        var multiple = new MultipleChoiceQuestion
+        {
+            CandidateAnswers = Enumerable.Range(2, RandomGenerator.NextInt(2, 6))
+                .Select(i => new MultipleChoiceAnswerItem { Value = RandomGenerator.NextString(0xff)})
+                .ToList()
+        };
+
+        Assert.IsTrue(_validator.Validate(multiple).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateMultipleChoiceQuestionShouldFail()
+    {
+        var multiple = new MultipleChoiceQuestion 
+        {
+            CandidateAnswers = Enumerable.Range(0, 1)
+                .Select(i => new MultipleChoiceAnswerItem { Value = RandomGenerator.NextString(0xff) })
+                .ToList()
+        };
+
+        var result = _validator.TestValidate(multiple);
+
+        result.ShouldHaveValidationErrorFor(l => l.CandidateAnswers.Count);
+    }
+
+    #endregion
+}
