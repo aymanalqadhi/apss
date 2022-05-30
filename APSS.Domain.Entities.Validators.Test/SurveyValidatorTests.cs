3fd265fcefafe9f2e0784500ca95b39efb48843b
wosama <almaktary.osama@gmail.com>
2022-05-30 23:39:27 +0300

    Test SurveyValidator class

---
+++ APSS.Domain.Entities.Validators.Test/SurveyValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentValidation.TestHelper;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class SurveyValidatorTests
+{
+    #region Private fields
+    
+    private readonly SurveyValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateSurveyShouldSucceed()
+    {
+        var survey = new Survey
+        {
+            Name = RandomGenerator.NextString(0xff),
+            CreatedBy = new User { AccessLevel = AccessLevel.Group},
+        };
+
+        Assert.IsTrue(_validator.Validate(survey).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateSurveyShouldFail()
+    {
+        var survey = new Survey 
+        {
+            Name = "",
+            CreatedBy = new User { AccessLevel = AccessLevel.Farmer},
+        };
+
+        var result = _validator.TestValidate(survey);
+
+        result.ShouldHaveValidationErrorFor(l => l.Name);
+        result.ShouldHaveValidationErrorFor(l => l.CreatedBy.AccessLevel);
+    }
+
+    #endregion
+}
