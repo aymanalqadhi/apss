6a3b88a09234792e90aa74af9a7cac885e83f9fa
wosama <almaktary.osama@gmail.com>
2022-05-30 21:16:53 +0300

    Test LandProductValidator class

---
+++ APSS.Domain.Entities.Validators.Test/LandProductValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentValidation.TestHelper;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class LandProductValidatorTests
+{
+    #region Private fields
+
+    private readonly LandProductValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateLandProductShouldSucceed()
+    {
+        var landProduct = new LandProduct
+        {
+            CropName = RandomGenerator.NextString(0xff),
+            Quantity = RandomGenerator.NextInt(1)
+        };
+
+        Assert.IsTrue(_validator.Validate(landProduct).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateLandProductShouldFail()
+    {
+        var landProduct = new LandProduct
+        {
+            CropName = "",
+            Quantity = 0
+        };
+
+        var result = _validator.TestValidate(landProduct);
+
+        result.ShouldHaveValidationErrorFor(a => a.CropName);
+        result.ShouldHaveValidationErrorFor(a => a.Quantity);
+    }
+
+    #endregion
+}
