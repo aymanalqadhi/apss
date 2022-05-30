1129252cb5204bd5af1ceffb5658af3927364b73
aymanalqadhi <alqd@protonmail.com>
2022-05-30 16:09:08 +0300

    Add an interface for logging service

---
+++ APSS.Domain.Services/ILogsService.cs
@@ -0,0 +1,55 @@
+﻿using APSS.Domain.Entities;
+
+namespace APSS.Domain.Services;
+
+public interface ILogsService
+{
+    /// <summary>
+    /// Asynchronously creates a log entry
+    /// </summary>
+    /// <param name="severity">The severity of the log</param>
+    /// <param name="message">The message of the log</param>
+    /// <param name="tags">Optional tags to be attached to the log</param>
+    /// <returns>The created log object</returns>
+    Task<Log> LogAsync(LogSeverity severity, string message, params string[] tags);
+
+    /// <summary>
+    /// Asynchronously creates a debug log entry
+    /// </summary>
+    /// <param name="message">The message of the log</param>
+    /// <param name="tags">Optional tags to be attached to the log</param>
+    /// <returns>The created log object</returns>
+    Task<Log> LogDebugAsync(string message, params string[] tags);
+
+    /// <summary>
+    /// Asynchronously creates an information log entry
+    /// </summary>
+    /// <param name="message">The message of the log</param>
+    /// <param name="tags">Optional tags to be attached to the log</param>
+    /// <returns>The created log object</returns>
+    Task<Log> LogInfoAsync(string message, params string[] tags);
+
+    /// <summary>
+    /// Asynchronously creates a warning log entry
+    /// </summary>
+    /// <param name="message">The message of the log</param>
+    /// <param name="tags">Optional tags to be attached to the log</param>
+    /// <returns>The created log object</returns>
+    Task<Log> LogWarningAsync(string message, params string[] tags);
+
+    /// <summary>
+    /// Asynchronously creates an error log entry
+    /// </summary>
+    /// <param name="message">The message of the log</param>
+    /// <param name="tags">Optional tags to be attached to the log</param>
+    /// <returns>The created log object</returns>
+    Task<Log> LogErrorAsync(string message, params string[] tags);
+
+    /// <summary>
+    /// Asynchronously creates a fatal log entry
+    /// </summary>
+    /// <param name="message">The message of the log</param>
+    /// <param name="tags">Optional tags to be attached to the log</param>
+    /// <returns>The created log object</returns>
+    Task<Log> LogFatalAsync(string message, params string[] tags);
+}
