72dcad5e50e43b791f9eded216967790f1a7ab4a
aymanalqadhi <alqd@protonmail.com>
2022-05-30 20:19:11 +0300

    Add a value type to represent a date/time range

---
+++ APSS.Domain.ValueTypes/DateTimeRange.cs
@@ -0,0 +1,38 @@
+﻿using APSS.Domain.ValueTypes.Exceptions;
+
+namespace APSS.Domain.ValueTypes;
+
+public struct DateTimeRange
+{
+    #region Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="start">The starting date/time of the range</param>
+    /// <param name="end">The ending date/time of the range</param>
+    public DateTimeRange(DateTime start, DateTime end)
+    {
+        if (start > end)
+            throw new InvalidDateTimeRangeException(start, end);
+
+        Start = start;
+        End = end;
+    }
+
+    #endregion
+    
+    #region Public properties
+
+    /// <summary>
+    /// Gets the start of the range
+    /// </summary>
+    public DateTime Start { get; }
+
+    /// <summary>
+    /// Gets the end of the range
+    /// </summary>
+    public DateTime End { get; }
+
+    #endregion
+}
