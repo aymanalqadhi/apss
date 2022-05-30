e190f48159a9d35580a21055fe62db3edde7906e
wosama <almaktary.osama@gmail.com>
2022-05-30 23:39:59 +0300

    Test UserValidator class

---
+++ APSS.Domain.Entities.Validators.Test/UserValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentValidation.TestHelper;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class UserValidatorTests
+{
+    #region Private fields
+    
+    private readonly UserValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateUserShouldSucceed()
+    {
+        var user = new User
+        {
+            Name = RandomGenerator.NextString(0xff),
+            NationalId = RandomGenerator.NextString(0xff),
+            AccessLevel = AccessLevel.Farmer,
+            SupervisedBy = new User { AccessLevel = AccessLevel.Group},
+        };
+
+        Assert.IsTrue(_validator.Validate(user).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateUserShouldFail()
+    {
+        var user = new User 
+        {
+            Name = "",
+            NationalId = "",
+            AccessLevel = AccessLevel.Root,
+            SupervisedBy = new User { AccessLevel = AccessLevel.Presedint},
+        };
+
+        var result = _validator.TestValidate(user);
+
+        result.ShouldHaveValidationErrorFor(l => l.Name);
+        result.ShouldHaveValidationErrorFor(l => l.NationalId);
+        result.ShouldHaveValidationErrorFor(l => l.AccessLevel);
+        result.ShouldHaveValidationErrorFor(l => l.SupervisedBy);
+    }
+
+    #endregion
+}
