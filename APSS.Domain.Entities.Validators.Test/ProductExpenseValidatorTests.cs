a09f5e3bb1483a048ba2dcf912f6e2748b9f3a1b
wosama <almaktary.osama@gmail.com>
2022-05-30 23:38:48 +0300

    Test ProductExpenseValidator class

---
+++ APSS.Domain.Entities.Validators.Test/ProductExpenseValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Entities.Validators;
+using APSS.Tests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using FluentValidation.TestHelper;
+
+namespace APSS.Tests.Domain.Entities.Validators;
+
+[TestClass]
+public class ProductExpenseValidatorTests
+{
+    #region Private fields
+    
+    private readonly ProductExpenseValidator _validator = new();
+
+    #endregion
+
+    #region Tests
+
+    [TestMethod]
+    public void ValidateProductExpenseShouldSucceed()
+    {
+        var expense = new ProductExpense
+        {
+            Price = Convert.ToDecimal(RandomGenerator.NextDouble(0, 1_000_000))
+        };
+
+        Assert.IsTrue(_validator.Validate(expense).IsValid);
+    }
+
+    [TestMethod]
+    public void ValidateProductExpenseShouldFail()
+    {
+        var expense = new ProductExpense 
+        {
+            Price = -2
+        };
+
+        var result = _validator.TestValidate(expense);
+
+        result.ShouldHaveValidationErrorFor(l => l.Price);
+    }
+
+    #endregion
+}
