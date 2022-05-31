37d2ae0aa1d7ea4abab466f9dcfa2efc09805147
aymanalqadhi <alqd@protonmail.com>
2022-05-31 12:59:38 +0300

    Add an exception to be thrown if invalid pagination parameters were used

---
+++ APSS.Domain.Repositories/Exceptions/InvalidPaginationParametersException.cs
@@ -0,0 +1,41 @@
+﻿namespace APSS.Domain.Repositories.Exceptions;
+
+public class InvalidPaginationParametersException : Exception
+{
+    #region Private fields
+
+    private readonly int _page;
+    private readonly int _pageLength;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="page">The page index</param>
+    /// <param name="pageLength">The page length</param>
+    public InvalidPaginationParametersException(int page, int pageLength)
+        : base($"invalid pagination parameters ({page}, {pageLength})")
+    {
+        _page = page;
+        _pageLength = pageLength;
+    }
+
+    #endregion
+
+    #region Public properties
+
+    /// <summary>
+    /// Gets the page index
+    /// </summary>
+    public int Page => _page;
+
+    /// <summary>
+    /// Gets the page length
+    /// </summary>
+    public int PageLength => _pageLength;
+
+    #endregion
+}
