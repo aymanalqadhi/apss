79a235b7813f23a7c6af9d9c29b1686215ac54be
aymanalqadhi <alqd@protonmail.com>
2022-05-31 11:23:31 +0300

    Add a utility factory that creates a testing unit of work object

---
+++ APSS.Tests.Infrastructure.Repositories.EntityFramework/TestUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using APSS.Domain.Repositories;
+using APSS.Infrastructure.Repositores.EntityFramework;
+
+namespace APSS.Tests.Infrastructure.Repositories.EntityFramework;
+
+public static class TestUnitOfWork
+{
+    /// <summary>
+    /// Creates a testing unit of work object
+    /// </summary>
+    /// <returns>The created object</returns>
+    public static IUnitOfWork Create()
+        => new ApssUnitOfWork(TestDbContext.Create());
+}
