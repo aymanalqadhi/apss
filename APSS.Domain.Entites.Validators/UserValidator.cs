ec4bdc343299c4957425a97097c932284f67bd94
aymanalqadhi <alqd@protonmail.com>
2022-06-02 14:55:44 +0300

    Update validation rules for User

--- APSS.Domain.Entites.Validators/UserValidator.cs
+++ APSS.Domain.Entites.Validators/UserValidator.cs
@@ -12,7 +12,7 @@ public sealed class UserValidator : Validator<User>
     /// </summary>
     public UserValidator()
     {
-        RuleFor(user => user.Name)
+        RuleFor(u => u.Name)
             .NotEmpty()
             .WithMessage("user name cannot be empty");
 
@@ -21,8 +21,14 @@ public sealed class UserValidator : Validator<User>
             .WithMessage("user national ID cannot be empty");
 
         RuleFor(u => u.SupervisedBy)
-            .Equal(null as User)
-            .When(u => u.AccessLevel == AccessLevel.Root);
+            .NotNull()
+            .When(u => u.AccessLevel != AccessLevel.Root)
+            .WithMessage("a user must have a supervisor");
+
+        RuleFor(u => u.SupervisedBy)
+            .Null()
+            .When(u => u.AccessLevel == AccessLevel.Root)
+            .WithMessage("a root user cannot have a supervisor");
 
         RuleFor(u => u.AccessLevel)
             .Equal(AccessLevel.Farmer)
@@ -46,11 +52,11 @@ public sealed class UserValidator : Validator<User>
 
         RuleFor(u => u.AccessLevel)
             .Equal(AccessLevel.Directorate)
-            .When(u => u.SupervisedBy!.AccessLevel == AccessLevel.City)
+            .When(u => u.SupervisedBy!.AccessLevel == AccessLevel.Governorate)
             .WithMessage("a directorate superviosr must be supervied by a city superviosr");
 
         RuleFor(u => u.AccessLevel)
-            .Equal(AccessLevel.City)
+            .Equal(AccessLevel.Governorate)
             .When(u => u.SupervisedBy!.AccessLevel == AccessLevel.Presedint)
             .WithMessage("a city superviosr must be supervied by the presedint");
 
