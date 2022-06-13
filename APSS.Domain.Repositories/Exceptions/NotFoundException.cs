6c190c59995da1c64bcfeb4627cbae32e99895cd
aymanalqadhi <alqd@protonmail.com>
2022-06-13 21:45:06 +0300

    Create an exception to be thrown when no matching values are found

---
+++ APSS.Domain.Repositories/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace APSS.Domain.Repositories.Exceptions;
+
+public sealed class NotFoundException : Exception
+{
+    /// <inheritdoc/>
+    public NotFoundException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+}
