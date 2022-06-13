9d8e7d059489c657e6d206d44c55c29637d77273
aymanalqadhi <alqd@protonmail.com>
2022-06-13 23:19:47 +0300

    Add an exception to be thrown when invalid date/time is used

---
+++ APSS.Domain.Services/Exceptions/InvalidDateTimeException.cs
@@ -0,0 +1,34 @@
+﻿namespace APSS.Domain.Services.Exceptions;
+
+public sealed class InvalidDateTimeException : Exception
+{
+    #region Fields
+
+    private readonly DateTime _dateTime;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="dateTime">The faulty date/time</param>
+    /// <param name="message">An optional message to clarify the error</param>
+    public InvalidDateTimeException(DateTime dateTime, string? message = null)
+        : base(message)
+    {
+        _dateTime = dateTime;
+    }
+
+    #endregion Public Constructors
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the faulty date/time
+    /// </summary>
+    public DateTime DateTime => _dateTime;
+
+    #endregion Properties
+}
