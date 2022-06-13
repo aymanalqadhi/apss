53d55658c8845eeb44702b3c344bd83eb166b3fe
aymanalqadhi <alqd@protonmail.com>
2022-06-13 23:16:51 +0300

    Add extension methods to get items by id to IQueryBuilder<T>

---
+++ APSS.Domain.Repositories.Extensions/QueryBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using APSS.Domain.Entities;
+
+namespace APSS.Domain.Repositories.Extensions;
+
+public static class QueryBuilderExtensions
+{
+    /// <summary>
+    /// An extension to <see cref="IQueryBuilder{T}"/> that allows getting items by id
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity of the builder</typeparam>
+    /// <param name="self">The query builder</param>
+    /// <param name="id">The id to look for</param>
+    /// <returns>A modified query builder</returns>
+    public static IQueryBuilder<TEntity> Find<TEntity>(this IQueryBuilder<TEntity> self, long id)
+        where TEntity : AuditableEntity
+    {
+        return self.Where(i => i.Id == id);
+    }
+
+    /// <summary>
+    /// An extension to <see cref="IQueryBuilder{T}"/> that allows asynchronously getting items by id
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity of the builder</typeparam>
+    /// <param name="self">The query builder</param>
+    /// <param name="id">The id to look for</param>
+    /// <returns>A modified query builder</returns>
+    public static Task<TEntity> FindAsync<TEntity>(this IQueryBuilder<TEntity> self, long id)
+        where TEntity : AuditableEntity
+    {
+        return self.Find(id).FirstAsync();
+    }
+}
