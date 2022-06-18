827976b7e3141574f6f3a47b4abf6df9c50d23bc
FarouqAleshawi <eng.farouq001@gmail.com>
2022-06-18 11:27:03 -0700

    add interface to represent the land

---
+++ APSS.Domain.Services/ILandService.cs
@@ -0,0 +1,38 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Repositories;
+
+namespace APSS.Domain.Services;
+
+public interface ILandService
+{
+    #region Public Methods
+
+    /// <summary>
+    /// Gets a query for the lands set
+    /// </summary>
+    /// <param name="UserId">The id of the user witch to get the land</param>
+    /// <returns></returns>
+    IQueryBuilder<Land> GetLands(long UserId);
+
+    /// <summary>
+    /// Asynchronously adds a new land
+    /// </summary>
+    /// <param name="userId">The id of the user who owns the land</param>
+    /// <param name="area">The place of the land</param>
+    /// <param name="longitude">The longitude of the land</param>
+    /// <param name="latitude">The latitude of the land</param>
+    /// <param name="address">The address of the land</param>
+    /// <param name="isUsable">Whether the land can be used or not</param>
+    /// <param name="isUsed">Whether the land is currently used or not</param>
+    /// <returns></returns>
+    Task<Land> AddLandAsync(
+        long userId,
+        long area,
+        double longitude,
+        double latitude,
+        string address,
+        bool isUsable = true,
+        bool isUsed = false);
+
+    #endregion Public Methods
+}
