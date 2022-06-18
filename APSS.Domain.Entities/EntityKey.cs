98b8a1f8c749249a250d9d89126ac4225da9c75d
wosama <almaktary.osama@gmail.com>
2022-06-18 19:11:22 +0300

    Add a type for strongly-typed entity keys

---
+++ APSS.Domain.Entities/EntityKey.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Domain.Entities;
+
+public record EntityKey<Entity, Tvalue>(ValueTask Id)
+{
+    /// <inheritence/>
+    public override string ToString() => Id?.ToString();
+}
+
+public sealed record EntityKey<TEntity>(long Id) : EntityKey<TEntity, long>(Id);
