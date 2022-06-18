b4e8f1fb6bdffad6e37bf5d37248f13ecb5295c8
wosama <almaktary.osama@gmail.com>
2022-06-18 22:19:05 +0300

    Add dedicated land product unit instead of string

---
+++ APSS.Domain.Entities/LandProductUnit.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Domain.Entities;
+
+public class LandProductUnit : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of animal product unit
+    /// </summary>
+    public string Name { get; set; } = null!;
+}
