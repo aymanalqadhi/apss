d7f829923c76ef2ba7e398b811b1117abefde9bd
wosama <almaktary.osama@gmail.com>
2022-06-18 20:20:13 +0300

    Remove strongly-typed key due to complexity of implentation

--- APSS.Domain.Entities/EntityKey.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APSS.Domain.Entities;
-
-public record EntityKey<Entity, Tvalue>(ValueTask Id)
-{
-    /// <inheritence/>
-    public override string ToString() => Id?.ToString();
-}
-
-public sealed record EntityKey<TEntity>(long Id) : EntityKey<TEntity, long>(Id);
