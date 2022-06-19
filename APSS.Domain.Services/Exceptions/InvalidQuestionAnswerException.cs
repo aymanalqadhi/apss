c5202a5e2045c331a6037d61df4115f6aff793f7
aymanalqadhi <alqd@protonmail.com>
2022-06-19 18:42:02 +0300

    Add an exception to be thrown when invalid quesiton answer is provided

---
+++ APSS.Domain.Services/Exceptions/InvalidQuestionAnswerException.cs
@@ -0,0 +1,46 @@
+﻿namespace APSS.Domain.Services.Exceptions;
+
+/// <summary>
+/// An exception to be thrown when an invalid question answer is found
+/// </summary>
+/// <typeparam name="TAnswer"></typeparam>
+public abstract class InvalidQuestionAnswerException<TAnswer> : Exception
+{
+    #region Fields
+
+    private readonly TAnswer _answer;
+    private readonly long _questionId;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="questionId">The id of the question having invalid answer</param>
+    /// <param name="answer">The faulty answer</param>
+    /// <param name="message">An optional messsage to clarify errors</param>
+    public InvalidQuestionAnswerException(long questionId, TAnswer answer, string? message)
+        : base(message)
+    {
+        _questionId = questionId;
+        _answer = answer;
+    }
+
+    #endregion Public Constructors
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the faulty answer
+    /// </summary>
+    public TAnswer Answer => _answer;
+
+    /// <summary>
+    /// Gets the id of the question
+    /// </summary>
+    public long QuestionId => _questionId;
+
+    #endregion Properties
+}
