99ac9ec2940fcae0f10cad3c681726384eaa8d28
wosama <almaktary.osama@gmail.com>
2022-07-11 18:26:34 +0300

    Add AnimalProductUnit validator

---
+++ APSS.Domain.Entites.Validators/AnimalProductUnitValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace APSS.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Account"/>
+/// </summary>
+public sealed class AnimalProductUnitValidator : Validator<AnimalProductUnit>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public AnimalProductUnitValidator()
+    {
+        RuleFor(u => u.Name)
+            .NotEmpty()
+            .WithMessage("animal product unit name cannot be empty");
+    }
+}
