db8bf7fe770b2e51fc8abed4b303bbc770b64bd4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-11 19:33:45 +0300

    Add an interface to be implemented by accounts service

---
+++ APSS.Domain.Services/IAccountsService.cs
@@ -0,0 +1,25 @@
+﻿using APSS.Domain.Entities;
+
+namespace APSS.Domain.Services;
+
+/// <summary>
+/// An interface to be implemented by accounts service
+/// </summary>
+public interface IAccountsService
+{
+    /// <summary>
+    /// Asynchrnously creates an account for a user
+    /// </summary>
+    /// <param name="superUserAccountId">The id of the account of the superuser of the user</param>
+    /// <param name="userId">The id of the user to add the account for</param>
+    /// <param name="holderName">The name of the holder of the account</param>
+    /// <param name="password">The password of the account</param>
+    /// <param name="permissions">The permissions of the account</param>
+    /// <returns>The created account object</returns>
+    Task<Account> CreateAccountAsync(
+        long superUserAccountId,
+        long userId,
+        string holderName,
+        string password,
+        PermissionType permissions);
+}
