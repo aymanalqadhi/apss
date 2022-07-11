c5eb7ddf82ee4b0376046cffba0fb84b6787d528
matari <matari@DESKTOP-LMAVEVC>
2022-07-11 20:13:19 +0300

    Test Population class

---
+++ APSS.Tests.Application.App/PopulationServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using APSS.Domain.Repositories;
+using APSS.Domain.Services;
+using System;
+
+namespace APSS.Tests.Application.App;
+
+public sealed class PopulationServiceTest : IDisposable
+{
+    #region Private fields
+
+    private readonly IUnitOfWork _uow;
+    private readonly IPopulationService _logsSvc;
+
+    #endregion Private fields
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
