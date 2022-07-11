20e4c3a08878ebc8d72f9abf9aac4c2ff3fa83f3
FarouqAleshawi <eng.farouq001@gmail.com>
2022-07-11 15:52:01 -0700

    Add an extention to get items with ownership validation

--- APSS.Domain.Repositories.Extensions/QueryBuilderSecurityExtensions.cs
+++ APSS.Domain.Repositories.Extensions/QueryBuilderSecurityExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using APSS.Domain.Entities;
 using APSS.Domain.Repositories.Extensions.Exceptions;
+using APSS.Domain.Services.Exceptions;
 
 namespace APSS.Domain.Repositories.Extensions;
 
@@ -36,5 +37,35 @@ public static class QueryBuilderSecurityExtensions
         }
 
         return account;
+    }
+
+    /// <summary>
+    /// Asynchronously gets an item with ownership validation
+    /// </summary>
+    /// <typeparam name="T">The type of the item</typeparam>
+    /// <param name="self"></param>
+    /// <param name="itemId">The id of the item to get</param>
+    /// <param name="account">The account of the item's owner</param>
+    /// <returns>The matching item</returns>
+    /// <exception cref="InsufficientPermissionsException">
+    /// Thrown if the accuont's user does not own the item
+    /// </exception>
+    public static async Task<T> FindWithOwnershipValidationAync<T>(
+        this IQueryBuilder<T> self,
+        long itemId,
+        Account account) where T : Ownable
+    {
+        var item = await self
+            .Include(i => i.OwnedBy)
+            .FindAsync(itemId);
+
+        if (account.User.Id != item.OwnedBy.Id)
+        {
+            throw new InsufficientPermissionsException(
+                account.Id,
+                $"user #{account.User.Id} with account #{account.Id} does not own {nameof(T)} #{itemId}");
+        }
+
+        return item;
     }
 }
