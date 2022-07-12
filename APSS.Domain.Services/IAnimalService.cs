666819deca2cd950d6195bb5428f41ce1f4b8525
baggashalkamali <bggashalkmali98@gmail.com>
2022-07-11 19:16:17 -1200

    Add interface for  animal group and animal product

---
+++ APSS.Domain.Services/IAnimalService.cs
@@ -0,0 +1,51 @@
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Repositories;
+
+namespace APSS.Domain.Services;
+
+public interface IAnimalService
+{
+
+
+	Task<AnimalGroup> AddAnimalGroupAsync(
+		long userId,
+		string type,
+		string name,
+		int quantity,
+		AnimalSex animalSex);
+
+
+	Task<AnimalProduct> AddAnimalProductAsync(
+		long userId,
+		long animalGroupId,
+		string name,
+		double quantity,
+		AnimalProductUnit unit,
+		TimeSpan periodTaken
+		);
+
+	Task<AnimalProduct> GetAnimalProductAsync(long farmerId, long animalProductId);
+
+	Task<IQueryBuilder<AnimalGroup>> GetAnimalGroupAsync(long AccountId, long animalGroupId);
+
+	Task<IQueryBuilder<AnimalGroup>> GetAllAnimalGroupAsync(long AccountId, long userId);
+
+
+
+	Task<AnimalProduct> UpdateAnimalProductAsync(long userId, AnimalProduct animalProduct);
+
+	Task DeleteAnimalProductAsync(long userId, long animalProductId);
+
+	
+
+	Task<AnimalGroup> UpdateAnimalGroupAsync(long userId, AnimalGroup animalGroup);
+	 
+	
+
+	Task DeleteAnimalGroupAsync(long userId, long animalGroupId);
+
+
+
+
+
+}
