da4b28c434cbe56539dc285736052fbd1cd3329d
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-13 21:15:04 +0300

    Add products' units repositories

--- APSS.Domain.Repositories/IUnitOfWork.cs
+++ APSS.Domain.Repositories/IUnitOfWork.cs
@@ -12,39 +12,9 @@ public interface IUnitOfWork : IDisposable, IAsyncDisposable
     IRepository<Account> Accounts { get; }
 
     /// <summary>
-    /// Gets the users repository
-    /// </summary>
-    IRepository<User> Users { get; }
-
-    /// <summary>
-    /// Gets the lands repository
-    /// </summary>
-    IRepository<Land> Lands { get; }
-
-    /// <summary>
     /// Gets the animal groups repository
     /// </summary>
     IRepository<AnimalGroup> AnimalGroups { get; }
-
-    /// <summary>
-    /// Gets the products repository
-    /// </summary>
-    IRepository<Product> Products { get; }
-
-    /// <summary>
-    /// Gets the produt expenses repository
-    /// </summary>
-    IRepository<ProductExpense> ProductExpenses { get; }
-
-    /// <summary>
-    /// Gets the land products repository
-    /// </summary>
-    IRepository<LandProduct> LandProducts { get; }
-
-    /// <summary>
-    /// Gets the seasons repository
-    /// </summary>
-    IRepository<Season> Sessions { get; }
 
     /// <summary>
     /// Gets the animal groups repositories
@@ -52,74 +22,9 @@ public interface IUnitOfWork : IDisposable, IAsyncDisposable
     IRepository<AnimalProduct> AnimalProducts { get; }
 
     /// <summary>
-    /// Gets the surveys repository
+    /// Gets the animal group products' units
     /// </summary>
-    IRepository<Survey> Surveys { get; }
-
-    /// <summary>
-    /// Gets the questions repository
-    /// </summary>
-    IRepository<Question> Questions { get; }
-
-    /// <summary>
-    /// Gets the multiple-choice questions repository
-    /// </summary>
-    IRepository<MultipleChoiceQuestion> MultipleChoiceQuestions { get; }
-
-    /// <summary>
-    /// Gets the multiple-choice questions' answer items repository
-    /// </summary>
-    IRepository<MultipleChoiceAnswerItem> MultipleChoiceAnswerItems { get; }
-
-    /// <summary>
-    /// Gets the logical questions repository
-    /// </summary>
-    IRepository<LogicalQuestion> LogicalQuestions { get; }
-
-    /// <summary>
-    /// Gets the text questions repository
-    /// </summary>
-    IRepository<TextQuestion> TextQuestions { get; }
-
-    /// <summary>
-    /// Gets the question answers repository
-    /// </summary>
-    IRepository<QuestionAnswer> QuestionAnswers { get; }
-
-    /// <summary>
-    /// Gets the multiple-choice question answers repository
-    /// </summary>
-    IRepository<MultipleChoiceQuestionAnswer> MultipleChoiceQuestionAnswers { get; }
-
-    /// <summary>
-    /// Gets the logical question answers repository
-    /// </summary>
-    IRepository<LogicalQuestionAnswer> LogicalQuestionAnswers { get; }
-
-    /// <summary>
-    /// Gets the text question answers repository
-    /// </summary>
-    IRepository<TextQuestionAnswer> TextQuestionAnswers { get; }
-
-    /// <summary>
-    /// Gets the survey entries repository
-    /// </summary>
-    IRepository<SurveyEntry> SurveyEntries { get; }
-
-    /// <summary>
-    /// Gets the individuals repository
-    /// </summary>
-    IRepository<Individual> Individuals { get; }
-
-    /// <summary>
-    /// Gets the skills repository
-    /// </summary>
-    IRepository<Skill> Skills { get; }
-
-    /// <summary>
-    /// Gets the volantaries repository
-    /// </summary>
-    IRepository<Voluntary> Volantaries { get; }
+    IRepository<AnimalProductUnit> AnimalProductUnits { get; }
 
     /// <summary>
     /// Gets the families repository
@@ -132,6 +37,36 @@ public interface IUnitOfWork : IDisposable, IAsyncDisposable
     IRepository<FamilyIndividual> FamilyIndividuals { get; }
 
     /// <summary>
+    /// Gets the individuals repository
+    /// </summary>
+    IRepository<Individual> Individuals { get; }
+
+    /// <summary>
+    /// Gets the land products repository
+    /// </summary>
+    IRepository<LandProduct> LandProducts { get; }
+
+    /// <summary>
+    /// Gets the land products' units
+    /// </summary>
+    IRepository<LandProductUnit> LandProductUnits { get; }
+
+    /// <summary>
+    /// Gets the lands repository
+    /// </summary>
+    IRepository<Land> Lands { get; }
+
+    /// <summary>
+    /// Gets the logical question answers repository
+    /// </summary>
+    IRepository<LogicalQuestionAnswer> LogicalQuestionAnswers { get; }
+
+    /// <summary>
+    /// Gets the logical questions repository
+    /// </summary>
+    IRepository<LogicalQuestion> LogicalQuestions { get; }
+
+    /// <summary>
     /// Gets the logs repository
     /// </summary>
     IRepository<Log> Logs { get; }
@@ -141,9 +76,91 @@ public interface IUnitOfWork : IDisposable, IAsyncDisposable
     /// </summary>
     IRepository<LogTag> LogTags { get; }
 
+    /// <summary>
+    /// Gets the multiple-choice questions' answer items repository
+    /// </summary>
+    IRepository<MultipleChoiceAnswerItem> MultipleChoiceAnswerItems { get; }
+
+    /// <summary>
+    /// Gets the multiple-choice question answers repository
+    /// </summary>
+    IRepository<MultipleChoiceQuestionAnswer> MultipleChoiceQuestionAnswers { get; }
+
+    /// <summary>
+    /// Gets the multiple-choice questions repository
+    /// </summary>
+    IRepository<MultipleChoiceQuestion> MultipleChoiceQuestions { get; }
+
+    /// <summary>
+    /// Gets the produt expenses repository
+    /// </summary>
+    IRepository<ProductExpense> ProductExpenses { get; }
+
+    /// <summary>
+    /// Gets the products repository
+    /// </summary>
+    IRepository<Product> Products { get; }
+
+    /// <summary>
+    /// Gets the question answers repository
+    /// </summary>
+    IRepository<QuestionAnswer> QuestionAnswers { get; }
+
+    /// <summary>
+    /// Gets the questions repository
+    /// </summary>
+    IRepository<Question> Questions { get; }
+
+    /// <summary>
+    /// Gets the seasons repository
+    /// </summary>
+    IRepository<Season> Sessions { get; }
+
+    /// <summary>
+    /// Gets the skills repository
+    /// </summary>
+    IRepository<Skill> Skills { get; }
+
+    /// <summary>
+    /// Gets the survey entries repository
+    /// </summary>
+    IRepository<SurveyEntry> SurveyEntries { get; }
+
+    /// <summary>
+    /// Gets the surveys repository
+    /// </summary>
+    IRepository<Survey> Surveys { get; }
+
+    /// <summary>
+    /// Gets the text question answers repository
+    /// </summary>
+    IRepository<TextQuestionAnswer> TextQuestionAnswers { get; }
+
+    /// <summary>
+    /// Gets the text questions repository
+    /// </summary>
+    IRepository<TextQuestion> TextQuestions { get; }
+
+    /// <summary>
+    /// Gets the users repository
+    /// </summary>
+    IRepository<User> Users { get; }
+
+    /// <summary>
+    /// Gets the volantaries repository
+    /// </summary>
+    IRepository<Voluntary> Volantaries { get; }
+
     #endregion Properties
 
-    #region Methods
+    #region Public Methods
+
+    /// <summary>
+    /// Asynchronously begins a transaction
+    /// </summary>
+    /// <param name="cancellationToken">The task cancellation token</param>
+    /// <returns></returns>
+    Task<IAsyncDatabaseTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Asynchronously commits changes to data backend
@@ -159,12 +176,5 @@ public interface IUnitOfWork : IDisposable, IAsyncDisposable
     /// <returns>The affected records count</returns>
     Task<int> CommitAsync(IAsyncDatabaseTransaction transaction, CancellationToken cancellationToken = default);
 
-    /// <summary>
-    /// Asynchronously begins a transaction
-    /// </summary>
-    /// <param name="cancellationToken">The task cancellation token</param>
-    /// <returns></returns>
-    Task<IAsyncDatabaseTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default);
-
-    #endregion Methods
+    #endregion Public Methods
 }
