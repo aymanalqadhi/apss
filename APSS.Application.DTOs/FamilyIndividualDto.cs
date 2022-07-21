483a07f301ed06c4dbdb7250b77f61ed63ccf801
matari <matari@DESKTOP-LMAVEVC>
2022-07-22 00:00:09 +0300

    Add class  dtos

---
+++ APSS.Application.DTOs/FamilyIndividualDto.cs
@@ -0,0 +1,9 @@
+﻿namespace APSS.Application.DTOs;
+
+public class FamilyIndividualDto
+{
+    public string FamilyName { get; set; } = null!;
+    public string NameIndividual { get; set; } = null!;
+    public bool IsParent { get; set; }
+    public bool IsProvider { get; set; }
+}
---
+++ APSS.Application.DTOs/FamlyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace APSS.Application.DTOs
+{
+    public class FamlyDto
+    {
+        public string Name { get; set; } = null!;
+        public string LivingLocation { get; set; } = null!;
+    }
+}
