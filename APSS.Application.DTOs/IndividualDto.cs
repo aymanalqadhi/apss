a50ef3339ece55f48e3f85909347351ba2798978
matari <matari@DESKTOP-LMAVEVC>
2022-07-22 00:00:09 +0300

    Add class  dtos

---
+++ APSS.Application.DTOs/IndividualDto.cs
@@ -0,0 +1,14 @@
+﻿namespace APSS.Application.DTOs;
+
+public class IndividualDto
+{
+    public string Name { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public string Job { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Sex { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+    public string NationalId { get; set; } = null!;
+    public string SocialStatus { get; set; } = null!;
+    public string AddedBy { get; set; } = null!;
+}
---
+++ APSS.Application.DTOs/SkillDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Application.DTOs
+{
+    public class SkillDto
+    {
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string Field { get; set; } = null!;
+        public string NameIndividual { get; set; } = null!;
+    }
+}
