ad4161b4ee722d01e927cc44abe36e5bee57862c
matari <matari@DESKTOP-LMAVEVC>
2022-07-22 19:36:39 +0300

    Add classes dto

---
+++ APSS.Web.Dto/FamilyIndividualDto.cs
@@ -0,0 +1,10 @@
+﻿namespace APSS.Web.Dto
+{
+    public class FamilyIndividualDto
+    {
+        public long Id { get; set; }
+        public string Individual { get; set; } = null!;
+        public bool IsProvider { get; set; } = false;
+        public bool IsParent { get; set; }
+    }
+}
