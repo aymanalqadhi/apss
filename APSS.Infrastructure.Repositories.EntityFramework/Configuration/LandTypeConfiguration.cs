4b19c776b98af64b892f55347929cb13f200223e
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-23 00:14:16 +0300

    Configure Land database model creation

---
+++ APSS.Infrastructure.Repositories.EntityFramework/Configuration/LandTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using APSS.Domain.Entities;
+
+namespace APSS.Infrastructure.Repositories.EntityFramework.Configuration;
+
+public sealed class LandTypeConfiguration : IEntityTypeConfiguration<Land>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<Land> builder)
+    {
+        builder
+            .HasMany(l => l.Products)
+            .WithOne(p => p.Producer)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
