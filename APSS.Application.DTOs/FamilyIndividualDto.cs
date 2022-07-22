747db7cb1dc4a86f79673be4f4a93f98a859528e
matari <matari@DESKTOP-LMAVEVC>
2022-07-22 19:35:11 +0300

    Delete APSS.Application.DTO

--- APSS.Application.DTOs/FamilyIndividualDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APSS.Application.DTOs;
-
-public class FamilyIndividualDto
-{
-    public string FamilyName { get; set; } = null!;
-    public string NameIndividual { get; set; } = null!;
-    public bool IsParent { get; set; }
-    public bool IsProvider { get; set; }
-}
