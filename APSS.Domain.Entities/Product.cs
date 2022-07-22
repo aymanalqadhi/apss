9627114d97e38282a4fbdc5555a5d5367c893d37
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-22 17:41:39 +0300

    Add AddedField to Product

--- APSS.Domain.Entities/Product.cs
+++ APSS.Domain.Entities/Product.cs
@@ -6,4 +6,9 @@ public abstract class Product : Confirmable
     /// Gets or sets the expenses spent on this product
     /// </summary>
     public ICollection<ProductExpense> Expenses { get; set; } = new List<ProductExpense>();
+
+    /// <summary>
+    /// Gets or sets the user who added the product
+    /// </summary>
+    public User AddedBy { get; set; } = null!;
 }
