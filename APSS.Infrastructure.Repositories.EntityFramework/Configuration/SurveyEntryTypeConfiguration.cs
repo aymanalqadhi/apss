7906038d17f576f05d8dbed6eb54fce87fb0e8d3
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-23 00:14:38 +0300

    Configure SurveyEntry database model creation

---
+++ APSS.Infrastructure.Repositories.EntityFramework/Configuration/SurveyEntryTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using APSS.Domain.Entities;
+
+namespace APSS.Infrastructure.Repositories.EntityFramework.Configuration;
+
+public sealed class SurveyEntryTypeConfiguration : IEntityTypeConfiguration<SurveyEntry>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<SurveyEntry> builder)
+    {
+        builder
+            .HasOne(s => s.MadeBy)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
