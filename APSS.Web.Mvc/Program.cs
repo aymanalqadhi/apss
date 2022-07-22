bcec5b14e543078b7e493b97025efeb1abe0d242
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-22 23:17:25 +0300

    Register services

--- APSS.Web.Mvc/Program.cs
+++ APSS.Web.Mvc/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using APSS.Application.App;
+using APSS.Domain.Repositories;
+using APSS.Domain.Services;
 using APSS.Infrastructure.Repositores.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,10 +10,25 @@ builder.Services.AddMvc();
 
 #region Services
 
+var svc = builder.Services;
+
 // Database context
-builder.Services.AddDbContext<ApssDbContext>(cfg =>
+svc.AddDbContext<ApssDbContext>(cfg =>
     cfg.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"])
 );
+
+// Unit-of-work
+svc.AddScoped<IUnitOfWork, ApssUnitOfWork>();
+
+// Services
+svc.AddScoped<ILogsService, DatabaseLogsService>();
+svc.AddScoped<IUsersService, UsersService>();
+svc.AddScoped<IPermissionsService, PermissionsService>();
+svc.AddScoped<IAccountsService, AccountsService>();
+svc.AddScoped<IAnimalService, AnimalService>();
+svc.AddScoped<ILandService, LandService>();
+svc.AddScoped<IPopulationService, PopulationService>();
+svc.AddScoped<ISurveysService, SurveysService>();
 
 #endregion Services
 
