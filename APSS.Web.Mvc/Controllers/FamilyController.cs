77d9774124f5becfa744c6d44064c3a69c022a98
matari <matari@DESKTOP-LMAVEVC>
2022-07-22 22:55:57 +0300

    Add FamilyController

---
+++ APSS.Web.Mvc/Controllers/FamilyController.cs
@@ -0,0 +1,23 @@
+﻿using APSS.Domain.Repositories;
+using APSS.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APSS.Web.Mvc.Controllers
+{
+    public class FamilyController : Controller
+    {
+        private readonly IPopulationService _pSvc;
+        private readonly IUnitOfWork _uow = new UnitOfWork();
+
+        public FamilyController(IPopulationService pSvc)
+        {
+            _pSvc = pSvc;
+        }
+
+        public IActionResult Indix()
+        {
+            var family = from famly in
+            return View();
+        }
+    }
+}
