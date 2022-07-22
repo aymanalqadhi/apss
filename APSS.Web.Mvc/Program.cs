3abc67b78ce80ddd9b336884554ea3ccb1d5ef77
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-22 23:00:59 +0300

    Restructure initializaiton code

--- APSS.Web.Mvc/Program.cs
+++ APSS.Web.Mvc/Program.cs
@@ -1,25 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+#region Services
+
+builder.Services.AddMvc();
+
+#endregion Services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Environmen-dependent settings
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHttpsRedirection();
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
 
 app.Run();
