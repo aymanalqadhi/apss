19443b731e465747ba7d1110e77eb2779bb616f5
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-23 00:14:09 +0300

    Configure FamilyIndividual database model creation

---
+++ APSS.Infrastructure.Repositories.EntityFramework/Configuration/FamilyIndividualTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using APSS.Domain.Entities;
+
+namespace APSS.Infrastructure.Repositories.EntityFramework.Configuration;
+
+public sealed class FamilyIndividualTypeConfiguration : IEntityTypeConfiguration<FamilyIndividual>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<FamilyIndividual> builder)
+    {
+        builder
+            .HasOne(f => f.Individual)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
