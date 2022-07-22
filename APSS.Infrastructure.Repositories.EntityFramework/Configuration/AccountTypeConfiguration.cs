a834393089fbc0f25967de6cf7604c8a9a7a6306
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-23 00:13:46 +0300

    Configure Account database model creation

---
+++ APSS.Infrastructure.Repositories.EntityFramework/Configuration/AccountTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using APSS.Domain.Entities;
+
+namespace APSS.Infrastructure.Repositories.EntityFramework.Configuration;
+
+public sealed class AccountTypeConfiguration : IEntityTypeConfiguration<Account>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        // relationships
+        builder
+            .HasOne(a => a.User)
+            .WithMany(a => a.Accounts)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        builder
+            .HasOne(a => a.AddedBy)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
