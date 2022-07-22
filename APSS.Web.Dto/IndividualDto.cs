e9082a2fb553c9730e34efca72f9f6761ad41667
matari <matari@DESKTOP-LMAVEVC>
2022-07-22 19:36:39 +0300

    Add classes dto

---
+++ APSS.Web.Dto/IndividualDto.cs
@@ -0,0 +1,16 @@
+﻿namespace APSS.Web.Dto
+{
+    public class IndividualDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Job { get; set; } = null!;
+        public string Sex { get; set; }= null!;
+        public string SocialStatus { get; set; } = null!;
+        public string PhonNumber { get; set; } = null!;
+        public string NationalId { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string User { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ APSS.Web.Dto/VoluntaryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Web.Dto
+{
+    public class VoluntaryDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string Field { get; set; } = null!;
+        public string NameIndividual { get; set; } = null!;
+    }
+}
