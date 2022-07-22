d8aa34b8a6aea696bb43fdb36665b46bb0004a68
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-23 00:14:30 +0300

    Configure ProductLand database model creation

---
+++ APSS.Infrastructure.Repositories.EntityFramework/Configuration/ProductLandTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using APSS.Domain.Entities;
+
+namespace APSS.Infrastructure.Repositories.EntityFramework.Configuration;
+
+public sealed class ProductLandTypeConfiguration : IEntityTypeConfiguration<Product>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder
+            .HasOne(p => p.AddedBy)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
