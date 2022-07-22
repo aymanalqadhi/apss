0ef13344a3247412c2acfe57451caf11a0249cee
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-22 19:50:15 +0300

    Add a base data-transfer object class

---
+++ APSS.Web.Dto/BaseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace APSS.Web.Dto;
+
+public abstract class BaseDto
+{
+    /// <summary>
+    /// Gets or sets the id of the entity
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the creation date of the entity
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+}
