809513563d4e234fe05dd003a98fdaf646c1bd80
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-23 00:14:22 +0300

    Configure ProductExpense database model creation

---
+++ APSS.Infrastructure.Repositories.EntityFramework/Configuration/ProductExpenseTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using APSS.Domain.Entities;
+
+namespace APSS.Infrastructure.Repositories.EntityFramework.Configuration;
+
+public sealed class ProductExpenseTypeConfiguration : IEntityTypeConfiguration<ProductExpense>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<ProductExpense> builder)
+    {
+        builder
+            .Property(s => s.Price)
+            .HasPrecision(18, 6);
+    }
+}
