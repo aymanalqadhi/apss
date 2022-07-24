73a41a5a2bfc31c540efd711796bbee0a944ab26
baggashalkamali <bggashalkmali98@gmail.com>
2022-07-23 21:55:31 -1200

    Add animalgroupDto

---
+++ APSS.Web.Dtos/AnimalGroupDto.cs
@@ -0,0 +1,19 @@
+﻿using APSS.Domain.Entities;
+using APSS.Web.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Web.Dtos
+{
+    internal class AnimalGroupDto : BaseAuditbleDto
+    {
+        public string Type { get; set; } = null!;
+
+        public int Quantity { get; set; }
+
+        public AnimalSex Sex { get; set; }
+    }
+}
