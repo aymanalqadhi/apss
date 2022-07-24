604bb3e297c6357497c151bdbaf000f9cdaf4316
baggashalkamali <bggashalkmali98@gmail.com>
2022-07-23 21:56:31 -1200

    Add animalproduct dto

---
+++ APSS.Web.Dtos/AnimalProductDto.cs
@@ -0,0 +1,23 @@
+﻿using APSS.Domain.Entities;
+using APSS.Web.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Web.Dtos
+{
+    internal class AnimalProductDto : BaseAuditbleDto
+    {
+        public string Name { get; set; } = null!;
+
+        public AnimalProductUnit Unit { get; set; } = null!;
+
+        public double Quantity { get; set; }
+
+        public TimeSpan PeriodTaken { get; set; }
+
+        public AnimalGroupDto Producer { get; set; } = null!;
+    }
+}
